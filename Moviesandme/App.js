0bd68d1957268e593f5d0976608b5c165efdc78d
Manny2296 <manny2296@gmail.com>
2019-11-28 15:49:42 +0100

    Add : Redux-persist,  To persist de Favorites and Avatar reducers. The estable version of redux-persist is 5.10.0 so we did downgrade this library cause the current VERSION IS 6.0.0

--- Moviesandme/App.js
+++ Moviesandme/App.js
@@ -3,15 +3,29 @@ import { StyleSheet, Text, View } from 'react-native';
 import Search from './components/Search.js'
 import Navigation from './navigation/Navigation.js';
 import { Provider } from 'react-redux'
+//import { store,persistor } from './store/configureStore'
+
 import Store from './store/configureStore'
+import { persistStore } from "redux-persist";
+import {  PersistGate } from "redux-persist/integration/react";
+
 export default function App() {
+  let persistor = persistStore(Store)
   return (
+    /**Il existe un site qui recense toutes les librairies React Native
+     *  avec un indicateur de qualité, leur compatibilité
+     *  (iOS, Android, Expo pour les CRNA, Web), etc.
+     *  Il s'agit du site Native Directory.
+     *  url : https://www.native.directory/ */
    /* <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View> */
     <Provider store={Store}>
+      <PersistGate loading={null} persistor={persistor}>
    <Navigation/>
+   </PersistGate>
     </Provider> 
+
     
   );
 }
--- Moviesandme/store/configureStore.js
+++ Moviesandme/store/configureStore.js
@@ -1,7 +1,23 @@
 import { createStore, combineReducers } from 'redux'
 import toggleFavorite from './reducers/favoriteReducer'
 import setAvatar from './reducers/avatarReducer'
-export default createStore(combineReducers({toggleFavorite,setAvatar}))
+import { persistCombineReducers, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import { persistStore } from "redux-persist";
+const rootPersistconf = {
+    key:'root',
+    storage,
+}
+const persistedReducer = persistReducer(rootPersistconf, toggleFavorite)
+const persistReduceravatar = persistReducer(rootPersistconf,setAvatar)
+const persistFinalReducer = persistCombineReducers(rootPersistconf, {toggleFavorite,setAvatar})
+export default createStore(persistFinalReducer)
+// export default () => {
+//     let store = createStore(persistFinalReducer)
+//     let persistor = persistStore(store)
+//     return {store,persistor}
+// }
 
 /**il faut utiliser la fonction  
  * combineReducers  de Redux pour ajouter plusieurs reducers au store */
